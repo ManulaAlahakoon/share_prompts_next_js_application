816e9c227999e643d242201ad3160ee86da4445c
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-05-04 13:27:14 +0530

    Desktop Navbar if sign out
    By importing getProviders from next-auth/react

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,66 +1,76 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import Image from 'next/image'
-import { useState, useEffect } from 'react';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  //const [providers, setProviders] = useState(null)
+  const [providers, setProviders] = useState(null);
   const isUserLoggedIn = true;
-/*
-  useEffect(() => { 
-    const setProviders = async () => {
-    const response = await getProviders()
-    setProviders(response)
-    }
-    setProviders();
-    }, [])
-  */
+
+  useEffect(() => {
+    const setProvidersFunc = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProvidersFunc();
+  }, []);
+
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href='/' className='flex gap-2 flex-center'>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src='/assets/images/logo.svg'
-          alt='Promptopia Logo'
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
           width={30}
           height={30}
-          className='object-contain'
+          className="object-contain"
         />
-        <p className='logo_text'>Promtopia</p>
+        <p className="logo_text">Promtopia</p>
       </Link>
 
       {/*Desktop Navigation*/}
 
-      <div className='sm:flex hidden'>
+      <div className="sm:flex hidden">
         {isUserLoggedIn ? (
-          <div className='flex gap-3 md:gap-5'>
-            <Link href='/create-propmt'
-              className='black_btn'>
-                Create Post
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-propmt" className="black_btn">
+              Create Post
             </Link>
-            <button type='button' onClick={signOut} className='outline_btn'>
+            <button type="button" onClick={signOut} className="outline_btn">
               Sign out
             </button>
-            <Link href='/profile'>
+            <Link href="/profile">
               <Image
                 src="/assets/images/profile.svg"
                 width={37}
                 height={37}
-                className='rounded-full'
-                alt='Profile'
+                className="rounded-full"
+                alt="Profile"
               />
             </Link>
           </div>
-        ):(<div></div>
-       
-        )
-        }
+        ) : (
+          <div>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="black_btn"
+                >
+                  Sign in
+                </button>
+              ))}
+          </div>
+        )}
       </div>
-
-
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
