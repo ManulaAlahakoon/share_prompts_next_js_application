0491b6cb1ffad71bd652bcdc2764040fd10a69b0
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-05-07 22:18:04 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,29 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
-import Nav from '@components/Nav'
-import Provider from '@components/Provider'
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover and Share AI Prompts"
-}
+  title: "Promptopia",
+  description: "Discover and Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
-        <div className='main'>
-          <div className='gradient'></div>
-        </div>
-        <main className="app">
-          <Nav></Nav>
-          {children}
-        </main>
-      </body>       
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav></Nav>
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
