6cef7c1e6090e3353db2cab07e26f5717fbb7510
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-05-28 22:24:46 +0530

    Update Feed.jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+'use client';
+
+import { useEffect, useState } from "react"
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (  // VERY IMPORTANT - DO NOT NOT NOT USE '{}' INSTEAD USE USE USE '()'
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+        ))}
+    </div>
+  )
+}
 
 const Feed = () => {
+
+  const [searchText, SetSearchText] = useState('')
+  const [posts, setPosts] = useState([])
+  
+  const handleSearchChange = (e) => {
+    
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/prompt')
+      const data = await res.json()
+
+      setPosts(data)
+    }
+
+    fetchPosts();
+  },[])
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>  
+
+      <PromptCardList
+        data={[posts]}
+        handleTagClick={()=>{}}
+      />
+    </section>
   )
 }
 
